4bc3ff6db22d5d823d4776ab317226a6cccf8a7f
ngil0001 <noahgilkey@gmail.com>
2025-08-31 15:00:30 -0400

    Added android logo with updated onCreate

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.businesscardapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
@@ -19,16 +24,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessLogo(modifier = Modifier)
                 }
             }
         }
     }
 }
+
+//two containers; middle of the screen and the bottom of the screen
+
+
+//top container
+@Composable
+fun BusinessLogo(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.android_logo)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
